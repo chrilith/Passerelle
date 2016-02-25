7c35dd5e8b6edf178f908ea8387e2cf50b944958
chrilith <chrilith@hotmail.com>
2016-02-25 20:31:47 +0100

    Cleanup

--- Lua Module/EntryPoint.cpp
+++ Lua Module/EntryPoint.cpp
@@ -1,19 +1,16 @@
 #include <Windows.h>
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
-{
-	switch (ul_reason_for_call)
-	{
-	case DLL_PROCESS_ATTACH:
-		DisableThreadLibraryCalls(hModule);	//Optimization since Lua thread threads weirdly...
-		break;
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		break;
+BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+	switch (ul_reason_for_call) {
+		case DLL_PROCESS_ATTACH:
+			// Optimization since Lua thread threads weirdly...
+			DisableThreadLibraryCalls(hModule);
+			break;
+		case DLL_THREAD_ATTACH:
+		case DLL_THREAD_DETACH:
+		case DLL_PROCESS_DETACH:
+			break;
 	}
+
 	return TRUE;
 }
